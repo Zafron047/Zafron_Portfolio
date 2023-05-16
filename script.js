35365de010bee7574d543c487cbeae3e5210aac2
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-05-16 23:10:58 +0600

    Fix linters error

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-let hamburgerLink = document.querySelector("#hamburger-link");
-let blurBackground = document.querySelector(".blur-background");
-let menuLayer = document.querySelector(".menu-layer");
+const hamburgerLink = document.querySelector('#hamburger-link');
+const blurBackground = document.querySelector('.blur-background');
+const menuLayer = document.querySelector('.menu-layer');
 
 function pops() {
-  blurBackground.style.display = "block";
-  menuLayer.style.display = "flex";
+  blurBackground.style.display = 'block';
+  menuLayer.style.display = 'flex';
 }
 
-hamburgerLink.addEventListener("click", pops);
+hamburgerLink.addEventListener('click', pops);
 
-let closeButton = document.querySelector(".close-button");
+const closeButton = document.querySelector('.close-button');
 
 function unPops() {
-  blurBackground.style.display = "none";
-  menuLayer.style.display = "none";
+  blurBackground.style.display = 'none';
+  menuLayer.style.display = 'none';
 }
 
-closeButton.addEventListener("click", unPops);
+closeButton.addEventListener('click', unPops);
 
-let Portfolio = document.querySelector("#Portfolio");
-let About = document.querySelector("#About");
-let Contact = document.querySelector("#Contact");
+const Portfolio = document.querySelector('#Portfolio');
+const About = document.querySelector('#About');
+const Contact = document.querySelector('#Contact');
 
-Portfolio.addEventListener("click", unPops);
-About.addEventListener("click", unPops);
-Contact.addEventListener("click", unPops);
+Portfolio.addEventListener('click', unPops);
+About.addEventListener('click', unPops);
+Contact.addEventListener('click', unPops);
