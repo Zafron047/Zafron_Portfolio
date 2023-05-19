961301d10b013279cc736b6f3e30758ec0f4d843
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-05-19 20:46:16 +0600

    Push Draft popup

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ Portfolio.addEventListener("click", unPops);
 About.addEventListener("click", unPops);
 Contact.addEventListener("click", unPops);
 
-// ----------- Project popup --------------
+// ----------- Mobile Project popup --------------
 
 const projectBtns = document.querySelectorAll("#portfolio-card #see-project");
 for (let i in projectBtns) {
@@ -94,19 +94,18 @@ for (let i in projectBtns) {
         </ul>
   
         <div class="pop-btn">
-  
-        <a href="${projectsArr[i].linkToLive}">
-          <button class="btn" href>
-            See Live
-            <i class="fa-solid fa-arrow-up-right-dots fa-2xs" style="color: #6070ff;"></i>
-          </button>
-        </a>
-        <a href="${projectsArr[i].linkToSource}">
-          <button class="btn">
-            See Source
-            <i class="fa-brands fa-github fa-2xs" style="color: #6070ff;"></i>
-          </button>
-        </a>
+          <a href="${projectsArr[i].linkToLive}">
+            <button class="btn" href>
+              See Live
+              <i class="fa-solid fa-arrow-up-right-dots fa-2xs" style="color: #6070ff;"></i>
+            </button>
+          </a>
+          <a href="${projectsArr[i].linkToSource}">
+            <button class="btn">
+              See Source
+              <i class="fa-brands fa-github fa-2xs" style="color: #6070ff;"></i>
+            </button>
+          </a>
         </div>
       </div>`;
     newDiv.classList.add("card");
@@ -128,4 +127,130 @@ function removeNewDiv() {
   parentElement.removeChild(newDiv);
 }
 
-// popupClose.addEventListener("click", removeNewDiv);
+// ----------- Desktop popup --------------
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 768) {
+    const projectBtnsDesktop = document.querySelectorAll(
+      "#portfolio-card #see-project"
+    );
+    for (let i in projectBtnsDesktop) {
+      projectBtnsDesktop[i].addEventListener("click", function desktopPop() {
+        const projectsArr = [
+          {
+            projectName: "Tonic",
+            projectDescription:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+            Technology: ["HTML", "CSS", "JavaScript"],
+            linkToLive:
+              "https://zafron047.github.io/Portfolio_Cohort-20_Mod-1/",
+            linkToSource:
+              "https://github.com/Zafron047/Portfolio_Cohort-20_Mod-1",
+          },
+          {
+            projectName: "Tonic",
+            projectDescription:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+            Technology: ["HTML", "CSS", "JavaScript"],
+            linkToLive:
+              "https://zafron047.github.io/Portfolio_Cohort-20_Mod-1/",
+            linkToSource:
+              "https://github.com/Zafron047/Portfolio_Cohort-20_Mod-1",
+          },
+          {
+            projectName: "Tonic",
+            projectDescription:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+            Technology: ["HTML", "CSS", "JavaScript"],
+            linkToLive:
+              "https://zafron047.github.io/Portfolio_Cohort-20_Mod-1/",
+            linkToSource:
+              "https://github.com/Zafron047/Portfolio_Cohort-20_Mod-1",
+          },
+          {
+            projectName: "Tonic",
+            projectDescription:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+            Technology: ["HTML", "CSS", "JavaScript"],
+            linkToLive:
+              "https://zafron047.github.io/Portfolio_Cohort-20_Mod-1/",
+            linkToSource:
+              "https://github.com/Zafron047/Portfolio_Cohort-20_Mod-1",
+          },
+        ];
+
+        const popDiv = document.createElement("div");
+        popDiv.id = "blur-background";
+        popDiv.classList.add("blur-background");
+        popDiv.style.position = "fixed";
+        popDiv.style.top = "0";
+        popDiv.style.left = "0";
+        popDiv.style.width = "100vw";
+        popDiv.style.height = "100vh";
+        popDiv.style.zIndex = "11";
+        popDiv.style.backdropFilter = "blur(6px)";
+        popDiv.innerHTML = `
+        <div class="d-card">
+          <div class="D-Pop-Header">
+            <div>
+              <h3 id="name">${projectsArr[i].projectName}</h3>
+              <div id="exp" class="exp">
+                <p>CANOPY</p>
+                <ul>
+                  <li>Back End Dev</li>
+                  <li>2015</li>
+                </ul>
+              </div>
+            </div>
+            <a><button class="popup-close" onclick="removeNewDiv">X</button></a>
+          </div>
+  
+          <img src="./assets/images/popup-img-desktop.svg"/>
+  
+          <div class="card-details">
+            <div class="card-details-left">
+              <p class="project-info">
+              ${projectsArr[i].projectDescription}
+              </p>
+            </div>
+  
+            <div class="card-details-right">
+              <ul class="skill">
+                <li class="skill-box">${projectsArr[i].Technology[0]}</li>
+                <li class="skill-box">${projectsArr[i].Technology[1]}</li>
+                <li class="skill-box">${projectsArr[i].Technology[2]}</li>
+              </ul>
+              <hr>
+              <div class="pop-btn">
+                <a href="${projectsArr[i].linkToLive}">
+                  <button class="d-pop-btn" href>
+                    See Live
+                    <i class="fa-solid fa-arrow-up-right-dots fa-2xs" style="color: #6070ff;"></i>
+                  </button>
+                </a>
+                <a href="${projectsArr[i].linkToSource}">
+                  <button class="d-pop-btn">
+                    See Source
+                    <i class="fa-brands fa-github fa-2xs" style="color: #6070ff;"></i>
+                  </button>
+                </a>
+              </div>
+            </div>  
+          </div>
+        </div>`;
+
+        document.body.appendChild(popDiv);
+      });
+    }
+  } else if (window.innerWidth < 768) {
+    
+  }
+});
+
+// ----------- close Desktop popup ------------
+
+function removeNewDiv() {
+  const newDiv = document.querySelector("#card");
+  const parentElement = newDiv.parentNode;
+  parentElement.removeChild(newDiv);
+}
